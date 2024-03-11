29e5402d09ea8a28253781043c4ccc8286b1aa0b
Karen Yip <karenyip1999@gmail.com>
2024-03-11 14:08:06 +0000

    Initial commit

---
+++ src/main/java/com/controllers/TopTrumpsController.java
@@ -0,0 +1,25 @@
+package com.example.top_trumps_start_code.controllers;
+
+import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.services.TopTrumpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/toptrumps")
+public class TopTrumpsController {
+
+    @Autowired
+    TopTrumpService topTrumpService;
+
+    @PostMapping
+    public ResponseEntity <String> showCards (@RequestBody ArrayList<Card> cards)
+    {
+        String reply = topTrumpService.checkWinner(cards.get(0), cards.get(1));
+        return new ResponseEntity<>(reply, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/services/TopTrumpService.java
@@ -0,0 +1,42 @@
+package com.example.top_trumps_start_code.services;
+
+import com.example.top_trumps_start_code.models.Card;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class TopTrumpService {
+
+    private ArrayList <Card> cards;
+
+    public TopTrumpService()
+    {
+
+    }
+
+    public String checkWinner(Card card1, Card card2)
+    {
+        if (card1.getCardValue() > card2.getCardValue())
+        {
+            return card1.getRank() + " of " + card1.getSuit() + " wins!";
+        }
+        if (card1.getCardValue() < card2.getCardValue())
+        {
+            return card2.getRank() + " of " + card2.getSuit() + " wins!";
+        }
+        if (card1.getRank() == card2.getRank() && card1.getSuit() == card2.getSuit())
+        {
+            return "You can't have the same card in deck!";
+        }
+        else return "It's a draw!";
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+}
